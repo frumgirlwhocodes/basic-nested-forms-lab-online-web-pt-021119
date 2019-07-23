c441ef72d3391456a0ca1eb57b72759a01f7a9c5
lieby weintraub <liebahinda@gmail.com>
2019-07-23 16:18:01 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,6 +13,12 @@ class RecipesController < ApplicationController
 
   def create
     @recipe=Recipe.new(recipe_params)
+     if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
   end
   
   private 
